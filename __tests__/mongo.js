ec67451670f4fc4ab2667a0a8f6b8fa9b8366a34
Évelyne Lachance <eslachance@gmail.com>
2019-03-18 01:56:33 -0400

    Add config example to mongo test

--- __tests__/mongo.js
+++ __tests__/mongo.js
@@ -2,6 +2,16 @@
 const Josh = require('../');
 const Mongo = require('../providers/mongo/');
 const config = require('./config-mongo.json');
+/* config-mongo.json example contents
+{
+  "name": "josh-jest",
+  "dbName": "testing",
+  "user": "josh",
+  "pass": "abcd1234",
+  "host": "mydbidentifier.mlab.com",
+  "port": 23456
+}
+*/
 
 describe('MongoDB Initialization', () => {
   it('should initialize with credentials', () => {
