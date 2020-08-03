6f462b9e603ecc4f0f96f4cc8dc4a60ca270dee0
Calum Henderson <calumproh28@gmail.com>
2020-08-03 23:02:48 +0100

    Added documentation.
    Added documentation for Set()
    Added documentation for Update()
    Added documentation for has()

--- .d.ts
+++ .d.ts
@@ -28,7 +28,18 @@ declare class josh {
     constructor(...args: any[]);
 
     dec(...args: any[]): void;
-
+    /**
+     * 
+     * @param key The key to delete.
+     * @see "Enmap Basics" - https://enmap.evie.dev/api#enmap-delete-key-path-enmap
+     * @example 
+     * // Wait until database is loaded then query.
+     * db.defer.then( () => {
+     *      db.set("FavouriteFruit", "Apple");
+     *      db.delete("FavouriteFruit"); // Deletes "FavouriteFruit" from the database
+     *      db.delete(db.all) // Deletes the entire database.
+     * })
+     */
     delete(key: string): void;
 
     ensure(...args: any[]): void;
@@ -37,15 +48,29 @@ declare class josh {
      * 
      * @param key The key to get. Returns undefined if the key does not exist.
      * @see "Enmap Basics" - https://enmap.evie.dev/api#enmap-get-key-path
-     * 
-     * @example db.defer.then(() => {
+     * @returns {any} Value
+     * @example 
+     * // Wait until database is loaded then query.
+     * db.defer.then( () => {
      *      db.set("FavouriteFruit", "Apple");
      *      db.get("FavouriteFruit") // Returns "Apple"
      *      db.get("WorstFruit") // Returns undefined because it doesn't exist.
      * })
      */
     get(key: string): any;
-
+    /**
+     * 
+     * @param key The key to check. Returns false if the key does not exist and true if it does.
+     * @see "Enmap Basics" - https://enmap.evie.dev/api#enmap-has-key-path-boolean
+     * @returns {Boolean} True/False
+     * @example 
+     * // Wait until database is loaded then query.
+     * db.defer.then( () => {
+     *      db.set("FavouriteFruit", "Apple");
+     *      db.has("FavouriteFruit") // Returns true
+     *      db.get("WorstFruit") // Returns false because it doesn't exist
+     * })
+     */
     has(key: string): void;
 
     inc(...args: any[]): void;
@@ -59,10 +84,36 @@ declare class josh {
     readyCheck(...args: any[]): void;
 
     remove(key: string): void;
-
+    /**
+     * 
+     * @param key The name of the key.
+     * @param value The value to set.
+     * @see "Enmap Basics" - https://enmap.evie.dev/api#enmap-set-key-val-path-enmap
+     * @example 
+     * // Wait until database is loaded then query.
+     * db.defer.then( () => {
+     *      db.set("FavouriteFruit", "Apple"); // Set "FavouriteFruit" to "Apple"
+     *      db.set("WorstFruits", ["Pineapple", "Bannana"]) // Set "WorstFruits" to an array.
+     *      db.get("WorstFruits.0") // Get WorstFruits[0], returns "Pineapple"
+     *      db.get("FavouriteFruit") // Get "FavouriteFruit," returns "Apple"
+     * })
+     */
     set(key: string, value: any): void;
-
-    update(...args: any[]): void;
+    /**
+     * 
+     * @param key The name of the key to merge.
+     * @param value The object to merge.
+     * @see "Josh Basics" - TODO: ADD URL TO UPDATE API METHOD
+     * @example 
+     * // Wait until database is loaded then query.
+     * db.defer.then( () => {
+     *      db.set("FavouriteFruit", {fruit: "Apple"}); // Set "FavouriteFruit" to "Apple"
+     *      db.get("FavouriteFruit") // Returns {fruit: "Apple"}
+     *      db.update("FavouriteFruit", {mouldy: true}) // Update "FavouriteFruit" to {fruit: "Apple", mouldy: true}
+     *      db.get("FavouriteFruit") // Returns {fruit: "Apple", mouldy: true}
+     * })
+     */
+    update(key: string, value: any): void;
 
     static providers: {
         mongo: providers.mongo;
