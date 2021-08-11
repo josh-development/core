6be231b1c35a50a2ffb314c614118a73c69dc5d0
RealShadowNova <real.shadow.nova@gmail.com>
2021-08-11 17:34:27 -0400

    feat: implement tests for `Josh` statics

---
+++ tests/Josh.test.ts
@@ -0,0 +1,15 @@
+import { Josh, MapProvider } from '../src';
+
+describe('Josh', () => {
+	describe('Statics', () => {
+		test('GIVEN defaultProvider THEN returns MapProvider', () => {
+			expect(Josh.defaultProvider).toBe(MapProvider);
+		});
+
+		test('GIVEN multi() THEN returns Josh instances', () => {
+			const { test } = Josh.multi(['test']);
+
+			expect(test).toBeInstanceOf(Josh);
+		});
+	});
+});
