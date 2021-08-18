d443613f37559232f41f3daa2c4a2c4a6c9d58d2
RealShadowNova <real.shadow.nova@gmail.com>
2021-08-18 16:38:29 -0400

    docs: implemented for `errors/JoshProviderError`

--- src/lib/errors/JoshProviderError.ts
+++ src/lib/errors/JoshProviderError.ts
@@ -1,7 +1,15 @@
 import type { Method } from '../types';
 import { JoshError } from './JoshError';
 
+/**
+ * The base class for {@link JoshProvider}.
+ * @since 2.0.0
+ */
 export class JoshProviderError extends JoshError {
+	/**
+	 * The method this error applies to.
+	 * @since 2.0.0
+	 */
 	public method: Method;
 
 	public constructor(options: JoshProviderError.Options) {
@@ -10,13 +18,25 @@ export class JoshProviderError extends JoshError {
 		this.method = options.method;
 	}
 
+	/**
+	 * The name for this error.
+	 * @since 2.0.0
+	 */
 	public get name() {
 		return 'JoshProviderError';
 	}
 }
 
 export namespace JoshProviderError {
+	/**
+	 * The options for {@link JoshProviderError}
+	 * @since 2.0.0
+	 */
 	export interface Options extends JoshError.Options {
+		/**
+		 * The method this error applies to.
+		 * @since 2.0.0
+		 */
 		method: Method;
 	}
 }
