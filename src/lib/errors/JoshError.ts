d19cfbefc188eb38637672e4363b9451f5b19506
RealShadowNova <real.shadow.nova@gmail.com>
2021-08-18 16:38:12 -0400

    docs: implemented for `errors/JoshError.ts`

--- src/lib/errors/JoshError.ts
+++ src/lib/errors/JoshError.ts
@@ -1,4 +1,12 @@
+/**
+ * The base class for errors in {@link Josh}
+ * @since 2.0.0
+ */
 export class JoshError extends Error {
+	/**
+	 * The identifier for this error.
+	 * @since 2.0.0
+	 */
 	public identifier: string;
 
 	public constructor(options: JoshError.Options) {
@@ -7,14 +15,30 @@ export class JoshError extends Error {
 		this.identifier = options.identifier;
 	}
 
+	/**
+	 * The name of this error.
+	 */
 	public get name() {
 		return 'JoshError';
 	}
 }
 
 export namespace JoshError {
+	/**
+	 * The options for {@link JoshError}
+	 * @since 2.0.0
+	 */
 	export interface Options {
+		/**
+		 * The identifier for this error.
+		 * @since 2.0.0
+		 */
 		identifier: string;
+
+		/**
+		 * The message for this error.
+		 * @since 2.0.0
+		 */
 		message?: string;
 	}
 }
