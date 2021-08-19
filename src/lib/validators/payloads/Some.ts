6da27f5a02593f8b5419c0bdf2bf538f5749f4f1
RealShadowNova <real.shadow.nova@gmail.com>
2021-08-18 21:47:13 -0400

    docs: implemented for `validators/*`

--- src/lib/validators/payloads/Some.ts
+++ src/lib/validators/payloads/Some.ts
@@ -1,10 +1,22 @@
 import { Payload, SomeByDataPayload, SomeByHookPayload, SomePayload } from '../../payloads';
 import { Method } from '../../types';
 
+/**
+ * Checks whether the given payload is a {@link SomeByDataPayload}
+ * @since 2.0.0
+ * @param payload The payload to check
+ * @returns Whether the check is `true` or `false`
+ */
 export function isSomeByDataPayload<Value = unknown>(payload: SomePayload<Value>): payload is SomeByDataPayload<Value> {
 	return payload.type === Payload.Type.Data && payload.method === Method.Some;
 }
 
+/**
+ * Checks whether the given payload is a {@link SomeByHookPayload}
+ * @since 2.0.0
+ * @param payload The payload to check
+ * @returns Whether the check is `true` or `false`
+ */
 export function isSomeByHookPayload<Value = unknown>(payload: SomePayload<Value>): payload is SomeByHookPayload<Value> {
 	return payload.type === Payload.Type.Hook && payload.method === Method.Some;
 }
--- src/lib/validators/payloads/Update.ts
+++ src/lib/validators/payloads/Update.ts
@@ -1,10 +1,22 @@
 import { Payload, UpdateByDataPayload, UpdateByHookPayload, UpdatePayload } from '../../payloads';
 import { Method } from '../../types';
 
+/**
+ * Checks whether the given payload is a {@link UpdateByDataPayload}
+ * @since 2.0.0
+ * @param payload The payload to check
+ * @returns Whether the check is `true` or `false`
+ */
 export function isUpdateByDataPayload<Value = unknown>(payload: UpdatePayload<Value>): payload is UpdateByDataPayload<Value> {
 	return payload.type === Payload.Type.Data && payload.method === Method.Update;
 }
 
+/**
+ * Checks whether the given payload is a {@link UpdateByHookPayload}
+ * @since 2.0.0
+ * @param payload The payload to check
+ * @returns Whether the check is `true` or `false`
+ */
 export function isUpdateByHookPayload<Value = unknown>(payload: UpdatePayload<Value>): payload is UpdateByHookPayload<Value> {
 	return payload.type === Payload.Type.Hook && payload.method === Method.Update;
 }
