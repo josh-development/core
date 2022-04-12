fbbee22ec35b735358241b922cc8986982675a68
RealShadowNova <real.shadow.nova@gmail.com>
2022-04-11 21:18:19 -0400

    refactor(middleware): context pt. 2

--- src/middlewares/AutoEnsure.ts
+++ src/middlewares/AutoEnsure.ts
@@ -103,9 +103,10 @@ export class AutoEnsure<StoredValue = unknown> extends Middleware<StoredValue> {
   }
 
   public async [Method.SetMany](payload: Payloads.SetMany): Promise<Payloads.SetMany> {
+    const { entries } = payload;
     const { defaultValue } = this.context;
 
-    for (const [{ key }] of payload.entries) await this.provider.ensure({ method: Method.Ensure, key, data: defaultValue, defaultValue });
+    for (const [{ key }] of entries) await this.provider.ensure({ method: Method.Ensure, key, data: defaultValue, defaultValue });
 
     return payload;
   }
