fb622e2d3205b891422b16968fe1a40df1d361d5
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-09-17 15:24:09 -0400

    feat!: remove `umd` bundle

--- rollup.config.ts
+++ rollup.config.ts
@@ -17,19 +17,6 @@ export default defineConfig({
       format: 'es',
       exports: 'named',
       sourcemap: true
-    },
-    {
-      file: './dist/index.umd.js',
-      format: 'umd',
-      name: 'JoshCore',
-      sourcemap: true,
-      globals: {
-        '@joshdb/auto-ensure': 'JoshMiddlewareAutoEnsure',
-        '@joshdb/map': 'JoshProviderMap',
-        '@joshdb/provider': 'JoshProvider',
-        '@sapphire/utilities': 'SapphireUtilities',
-        process: 'Process'
-      }
     }
   ],
   external: ['@joshdb/auto-ensure', '@joshdb/map', '@joshdb/provider', '@sapphire/utilities', 'process'],
