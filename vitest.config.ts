992573eea00eca9479e858495d3ac98473955ddb
DanCodes <dan@dancodes.online>
2023-04-16 22:15:11 +0100

    fix(typescript): lock provider version

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    deps: {
-      inline:
-        // TODO: Replace with true once https://github.com/vitest-dev/vitest/issues/2806 is fixed.
-        [/^(?!.*vitest).*$/]
-    },
+    // deps: {
+    //   inline:
+    //     // TODO: Replace with true once https://github.com/vitest-dev/vitest/issues/2806 is fixed.
+    //     // [/^(?!.*vitest).*$/]
+    // },
     globals: true,
     coverage: {
       enabled: true,
